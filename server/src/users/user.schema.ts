ff17684df1c74386cb764c3de360d71eb369243a
Barok Dagim <dagimbarok@gmail.com>
2024-05-15 19:10:28 +0300

    fix: import error

--- server/src/users/user.schema.ts
+++ server/src/users/user.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import bcrypt from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 
 export type UserDocument = HydratedDocument<User>;
 
