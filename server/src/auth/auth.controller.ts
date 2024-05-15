816c27fc9aa46683a4f43bc1f25814870ba9a6c6
Barok Dagim <dagimbarok@gmail.com>
2024-05-15 18:19:16 +0300

    refactor: remove unused import

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,7 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
-import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
