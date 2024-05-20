daf52876d2a34cb74053cd630a2d5e16c2d643e5
Barok Dagim <dagimbarok@gmail.com>
2024-05-20 16:01:08 +0300

    feat: pagination basic styles

--- client/src/components/ActivePhishes.tsx
+++ client/src/components/ActivePhishes.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import Pagination from "rc-pagination";
 
 import PhishCard from "./PhishCard";
+import StyledPagination from "./StyledPagination";
 import { LocalStorageKeys, getItemFromLocalStorage } from "../lib/localStorage";
 import { Collection, Phish as PhishType } from "../lib/types";
 
@@ -66,8 +66,18 @@ export default function ActivePhishes() {
   }
 
   return (
-    <div className="p-10">
+    <div className="p-10 flex flex-col gap-5">
       {isFetching && <p className="mb-2">Updating...</p>}
+
+      <button
+        className="self-end bg-gray-300 px-2 py-1 text-sm"
+        onClick={() => {
+          refetch();
+        }}
+      >
+        Refresh
+      </button>
+
       <ol className="grid grid-cols-3 gap-10">
         {phishes?.data.map((phish) => (
           <li key={phish._id}>
@@ -76,11 +86,12 @@ export default function ActivePhishes() {
         ))}
       </ol>
 
-      <Pagination
+      <StyledPagination
         current={page}
         pageSize={5}
         total={phishes.count}
         onChange={(newPage) => setPage(newPage)}
+        className="self-center"
       />
     </div>
   );
---
+++ client/src/components/StyledPagination/index.tsx
@@ -0,0 +1,7 @@
+import Pagination, { PaginationProps } from "rc-pagination";
+
+import "./index.css";
+
+export default function StyledPagination(props: PaginationProps) {
+  return <Pagination {...props} />;
+}
